4d42fdbc40c411e55de6f0efecefe9b332928620
Tina <Tina@macusers-MacBook-Pro-4.local>
2021-03-24 10:11:27 -0500

    connected postres to model file

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,23 @@
 class Question
+  DB = PG.connect({:host => "localhost", :port => 5432, :dbname => 'jeopardy_development'})
     def self.all
-      [
-        { name: 'Darth Vader', age:45 },
-        { name: 'Darth', age:65 },
-        { name: 'Vader', age:25 }
-      ]
+      results = DB.exec("SELECT * FROM questions;")
+      return results.map do |result|
+       {
+          "id" => result["id"].to_i,
+          "question" => result["question"],
+          "points" => result["points"].to_i,
+          "category" => result["category"],
+          "answer" => result["answer"]
+        }
+      end
+      # results.each do |result|
+      #   puts result
+      # end
+      # [
+      #   { name: 'Darth Vader', age:45 },
+      #   { name: 'Darth', age:65 },
+      #   { name: 'Vader', age:25 }
+      # ]
     end
 end
