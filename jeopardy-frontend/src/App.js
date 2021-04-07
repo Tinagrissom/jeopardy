54706b7b252220e43f772a52d3715a96f051d58a
Tina <Tina@macusers-MBP-4.attlocal.net>
2021-04-06 19:26:00 -0500

    rendering 200 point questions on homepage

--- jeopardy-frontend/src/App.js
+++ jeopardy-frontend/src/App.js
@@ -4,13 +4,36 @@ import axios from 'axios'
 class App extends Component {
 
   state = {
+    question: '',
+    points: '',
+    category: '',
+    answer: '',
     questions: [],
+  }
+
+  getQuestions = () => {
+    axios
+      .get('/questions')
+      .then(
+        (response) => this.setState({questions: response.data, question: '', points: '', category: '', answer: ''}),
+      )
+      .catch((error) => console.error(error))
+  }
+
+  componentDidMount = () => {
+    this.getQuestions()
   }
 
   render = () => {
     return (
       <div>
         <h1>Jeopardy</h1>
+        {this.state.questions.map((question) => {
+          return (
+          <h2>{question.question}</h2>
+        )
+        })}
+
       </div>
     )
   }
