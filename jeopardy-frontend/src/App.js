aeaaf91a62ac6c1a15058de167fc6a48fec17fd7
Tina <Tina@macusers-MacBook-Pro-4.local>
2021-04-08 12:51:52 -0500

    most current version

--- jeopardy-frontend/src/App.js
+++ jeopardy-frontend/src/App.js
@@ -40,35 +40,44 @@ class App extends Component {
   render = () => {
     return (
       <div>
+      {/* HEADER */}
         <div id="title">
             <button type="button" id="open" onClick={this.revealCategories}>
             LET'S PLAY
             </button>
         </div>
+
+        {/* GAMEBOARD */}
         <div className="game-board">
-            <div>
-              {this.state.reveal === true ?
-                <div className="categories" style={{
-                  backgroundColor:"#060DE3",
-                  margin:"0",
-                  backgroundSize:"cover",
-                }}>
-                  {this.state.questions.map((category, i) => {
-                      if (i === 0) {
-                      return (
-                        <h2>{category.category}</h2>
-                      )
-                    }
+
+          {/* REVEAL CATEGORIES */}
+              <div>
+                {this.state.reveal === true ?
+                  <div className="categories" style={{
+                    backgroundColor:"#060DE3",
+                    margin:"0",
+                    backgroundSize:"cover",
+                  }}>
+                    {this.state.questions.map((category, i) => {
+                        if (i === 0) {
+                        return (
+                          <h2>{category.category}</h2>
+                        )
+                      }
                   })}
-                </div>
-            : <div className="categories" style={{
-              backgroundImage: "url(https://www.newscaststudio.com/wp-content/uploads/2019/09/jeopardy-season-36-open.jpg)",
-              backgroundPosition: "center",
-              backgroundRepeat: "no-repeat",
-              backgroundSize: "cover",
-            }}></div> }
+              </div>
+            : <div
+                className="categories"
+                style={{
+                  backgroundImage: "url(https://www.newscaststudio.com/wp-content/uploads/2019/09/jeopardy-season-36-open.jpg)",
+                  backgroundPosition: "center",
+                  backgroundRepeat: "no-repeat",
+                  backgroundSize: "cover",
+                  }}>
+              </div> }
             </div>
 
+        {/* LOADING THE QUESTIONS */}
         {this.state.questions.map((question) => {
           return (
             <div className="questions">
